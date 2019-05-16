b6f71d59c093e781ecfbc2f79f6a04a1ecded09c
Ebuka <ebukaa@protonmail.com>
2019-05-16 13:30:44 +0100

    Modified

--- Climat/WeatherDataModel.swift
+++ Climat/WeatherDataModel.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class WeatherDataModel {
 
-    //Declare your model variables here
+    //Declare your model variables here 
     var temperature: Int = 0
     var condition: Int = 0
     var city: String = ""
